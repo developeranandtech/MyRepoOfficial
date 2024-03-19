a59ae293d1beab4b8a9237939103530f089f799e
developeranandtech <163002965+developeranandtech@users.noreply.github.com>
2024-03-19 12:17:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -377,28 +377,3 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-async def get_shortlink(link):
-    https = link.split(":")[0]
-    if "http" == https:
-        https = "https"
-        link = link.replace("http", https)
-
-    url = f'https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'
-    params = {'token': URL_SHORTNER_WEBSITE_API,
-              'link': link,
-              'format': 'json'
-              }
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json(content_type='text/html')
-                if data["status"] == "success":
-                    return data['shortlink']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return f'https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'
-
-    except Exception as e:
-        logger.error(e)
-        return f'https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'
